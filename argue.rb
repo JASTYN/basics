bd396a3c18ca3e7db41e20e2496769bfab804cef
JASTYN <jastynmanya@gmail.com>
2017-03-13 12:36:06 +0300

    new ones

---
+++ argue.rb
@@ -0,0 +1,5 @@
+def add(num1,num2,num3)
+	num1+num2+num3
+end
+
+puts add(10,20,30)
---
+++ cond.rb
@@ -0,0 +1,12 @@
+x = 10
+y = 100
+z = 10
+if x==y
+	puts "x is the same as y"
+elsif x>z
+	puts"x is greater thsn z"
+else 
+	puts "something else"
+		
+end
+	
