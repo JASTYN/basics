8bff97456203a8c331b18d865bbb1bccab3f1327
JASTYN <jastynmanya@gmail.com>
2017-04-02 13:20:40 +0300

    kujaribu tuu

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello world am doing programming\n");
+	/* code */
+	return 0;
+}
--- index.rb
+++ index.rb
@@ -1,6 +1,16 @@
 puts "Enter first name"
 fname=gets.chomp
+if
+  fname =="jastyn"
+  puts "you can proceed"
+elsif
+  fname=="JASTYN"
+  print"login the other user"
 
+else
+  puts"HINT NAME"
+
+end
 puts "Enter second name"
 sname=gets.chomp
 
@@ -12,6 +22,3 @@ puts "hey welcome to ubuntu i would like to know you"
 END{
 puts "wow! now i have known you"
 }
-
-
-
